2c540d30042b4a2bf13754eba23daa95fdf67d96
Bob Cousins <bobcousins42@googlemail.com>
2013-06-15 00:31:58 +0100

    Move code from app_main() to main.c

--- R2C2-RepRap_firmware/app/mendel.c
+++ R2C2-RepRap_firmware/app/mendel.c
@@ -314,24 +314,4 @@ void PrinterTask( void *pvParameters )
   9.  system is now ready to accept general GCode commands
 */
 
-void app_main (void)
-{
-  LW_RTOS_RESULT res;
-  /* Create the main system task. 
-  *  NB if using FreeRTOS: our system timer tick is called from FreeRTOS timer tick, which only runs after scheduler has started.
-  *  Therefore, we start only PrinterTask to do initialisation which requires timer, namely the FatFs/SD code.
-  */
-  //TODO: Check stack usage
-  
-  
-  res = lw_TaskCreate ( printer_task_init, printer_task_poll,  "Print", 512, ( void * ) NULL, LWR_IDLE_PRIORITY, NULL );
-  if (res != LWR_OK)
-    debug ("error starting PrinterTask\n");
-
-  /* Start the scheduler. */
-  lw_TaskScheduler();
-
-  /* should not get here */
-}
-
 
